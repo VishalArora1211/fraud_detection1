8d9aa9206e384dbdd542c106c3948619911ff92a
VishalArora1211 <india.vishal.arora@gmail.com>
2023-07-16 12:26:53 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("Adding all ML mode code here")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
